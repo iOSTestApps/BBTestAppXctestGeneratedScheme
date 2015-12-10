1c10b1c8d696fb388c4c05f8a13ce1acf4d56320
Christopher Stott <christopherstott@gmail.com>
2015-12-10 10:38:45 -0800

    Initial Commit

---
+++ BBTestAppXctestGeneratedScheme/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBTestAppXctestGeneratedScheme
+//
+//  Created by Chris on 2015-12-10.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BBTestAppXctestGeneratedScheme/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BBTestAppXctestGeneratedScheme
+//
+//  Created by Chris on 2015-12-10.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
